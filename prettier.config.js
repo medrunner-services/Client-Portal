bacf69fb9ed799c153ae32816877eebce6eafa47
Florian Sylvain <floriansylvainpro@gmail.com>
2023-01-14 23:37:51 +0100

    🎉 Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    arrowParens: "avoid",
+    tabWidth: 4,
+    printWidth: 80,
+    trailingComma: "all",
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <main>
+        <h1 class="text-3xl font-bold underline">
+            Welcome to the Medrunner Portal
+        </h1>
+    </main>
+</template>
