1d9302552ef2b847df96a9bdc7fd21729e8429d7
ElliotThiebaut <elliotthiebaut1@gmail.com>
2023-01-15 22:59:35 -0500

    :sparkles: Added login view and user store

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
             name: "home",
             component: HomeView,
         },
+        {
+            path: "/login",
+            name: "login",
+            component: ()=>import('@/views/LoginView.vue'),
+        },
     ],
 });
 
