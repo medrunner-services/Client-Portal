5d9b16af3e600d43c598c756e2d95ca02643cc09
Elliot Thiebaut <captain@medrunner.dev>
2023-05-28 00:42:06 -0400

    CP-48 Added loading screen for router

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -3,6 +3,7 @@ import axios from "axios";
 import { onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
+import Loader from "@/components/Loader.vue";
 import { initializeApi } from "@/utils/medrunnerClient";
 
 const route = useRoute();
@@ -46,14 +47,5 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="w-full h-[70vh] flex justify-center items-center">
-        <svg class="animate-spin h-14 w-14 text-primary-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"></circle>
-            <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-        </svg>
-    </div>
+    <Loader class="w-full h-[70vh] flex justify-center items-center" />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { useI18n } from "vue-i18n";
 import EmergencyForm from "@/components/EmergencyForm.vue";
 import EmergencyHistory from "@/components/EmergencyHistory.vue";
 import EmergencyTracking from "@/components/EmergencyTracking.vue";
+import Loader from "@/components/Loader.vue";
 import { useEmergencyStore } from "@/stores/emergencyStore";
 import { useUserStore } from "@/stores/userStore";
 import { api } from "@/utils/medrunnerClient";
@@ -124,16 +125,7 @@ const isLastPageHistory = computed(() => {
             <div v-if="loaded && activePage.length > 0">
                 <EmergencyHistory v-for="emergency in activePage" :key="emergency.creationTimestamp" class="mt-4 first:mt-0" :emergency="emergency" />
             </div>
-            <div v-else-if="!loaded" class="w-full flex justify-center items-center h-80">
-                <svg class="animate-spin h-14 w-14 text-primary-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"></circle>
-                    <path
-                        class="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                </svg>
-            </div>
+            <Loader v-else-if="!loaded" class="w-full flex justify-center items-center h-80" />
             <div v-else>
                 <p>{{ t("home_noEmergencies") }}</p>
             </div>
