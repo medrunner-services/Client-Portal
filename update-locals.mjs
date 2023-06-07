5d5bf1a0d2a92907cdb3b813375430af10950c8f
Elliot Thiebaut <captain@medrunner.dev>
2023-06-07 17:55:50 -0400

    Added script automation

---
+++ update-locals.mjs
@@ -0,0 +1,82 @@
+import fs from 'fs';
+import yauzl from 'yauzl';
+import { pipeline } from 'stream';
+import path from "path";
+
+
+const downloadLocals = async () => {
+	const response = await fetch('https://crowdin.com/api/v2/projects/564465/bundles/3/exports', {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer 6f68907200e7d7974c67b3b5936533576e8da5bc143817472f1a77287fec6f51ee487e4659ca32ba",
+		}
+	});
+
+	const parsedResponse = await response.json();
+
+	await new Promise(resolve => setTimeout(resolve, 5000))
+
+	const responseDownload = await fetch(`https://crowdin.com/api/v2/projects/564465/bundles/3/exports/${parsedResponse.data.identifier}/download`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer 6f68907200e7d7974c67b3b5936533576e8da5bc143817472f1a77287fec6f51ee487e4659ca32ba",
+		}
+	});
+
+	const parsedResponseDownload = await responseDownload.json();
+	const responseFile = await fetch(parsedResponseDownload.data.url)
+
+	await new Promise((resolve, reject) => {
+		pipeline(responseFile.body, fs.createWriteStream('src/locales/newLocales.zip'), (err) => {
+			if (err) {
+				console.log('Error saving: ' + err)
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
+
+	await new Promise((resolve, reject) => {
+		yauzl.open('src/locales/newLocales.zip', {lazyEntries: true}, (err, zipfile) => {
+			if (err) {
+				console.log('Error extracting: ' + err)
+				reject(err);
+				return;
+			}
+
+			zipfile.readEntry();
+			zipfile.on('entry', (entry) => {
+				zipfile.openReadStream(entry, (err, readStream) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+
+					const filePath = path.join('src/locales', entry.fileName);
+					const writeStream = fs.createWriteStream(filePath);
+					readStream.pipe(writeStream);
+
+					writeStream.on('close', () => {
+						zipfile.readEntry();
+					});
+				});
+			});
+
+			zipfile.on('end', () => {
+				resolve();
+			});
+
+			zipfile.on('error', (err) => {
+				console.log('Error extracting: ' + err)
+				reject(err);
+			});
+		});
+	});
+
+	await fs.unlinkSync('src/locales/newLocales.zip')
+}
+
+downloadLocals().then(() => console.log('🚀 Done'))
