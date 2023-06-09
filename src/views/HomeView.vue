3502f253b82c263f5c11b6839726970006491ace
Elliot Thiebaut <captain@medrunner.dev>
2023-06-08 22:49:06 -0400

    CP-71 Add "get help" button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -176,7 +176,19 @@ const isLastPageHistory = computed(() => {
                 @completed-tracked-emergency="completeEmergency"
                 @complete-emergency="completeEmergency(emergencyStore.trackedEmergency)"
             />
+
             <EmergencyForm v-else />
+
+            <a
+                href="https://discord.gg/medrunner"
+                target="_blank"
+                class="flex text-primary-900 font-Inter font-semibold mt-5 border-b-2 border-primary-900 w-fit items-center"
+            >
+                <span>{{ t("home_GetHelp") }}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+            </a>
         </div>
     </div>
 </template>
