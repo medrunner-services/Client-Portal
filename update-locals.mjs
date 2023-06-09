83e88321a31791ba42bbec9109ac1bc587106597
Elliot Thiebaut <elliotthiebaut1@gmail.com>
2023-06-08 22:59:54 -0400

    Nightly (#59)
    * v1.1.2 (#56) (#57)
    
    * Deleted navbar from AuthView
    
    * CP-62 Better complexe translation strings
    
    * Updated locals
    
    * Fixed currentLink in navbar
    
    * CP-57 Check WS stability on mobile
    
    * Bumped version
    
    * Fixed responsive issues
    
    * Added script automation
    
    * Updated locales script
    
    * CP-71 Add "get help" button

---
+++ update-locals.mjs
@@ -0,0 +1,85 @@
+import fs from 'fs';
+import yauzl from 'yauzl';
+import { pipeline } from 'stream';
+import path from "path";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.development' });
+
+
+const downloadLocals = async () => {
+	const response = await fetch('https://crowdin.com/api/v2/projects/564465/bundles/3/exports', {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${process.env.CROWDIN_TOKEN}`,
+		}
+	});
+
+	const parsedResponse = await response.json();
+
+	await new Promise(resolve => setTimeout(resolve, 5000))
+
+	const responseDownload = await fetch(`https://crowdin.com/api/v2/projects/564465/bundles/3/exports/${parsedResponse.data.identifier}/download`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${process.env.CROWDIN_TOKEN}`,
+		}
+	});
+
+	const parsedResponseDownload = await responseDownload.json();
+	const responseFile = await fetch(parsedResponseDownload.data.url)
+
+	await new Promise((resolve, reject) => {
+		pipeline(responseFile.body, fs.createWriteStream('src/locales/newLocales.zip'), (err) => {
+			if (err) {
+				console.log('Error saving: ' + err)
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
+
+	await new Promise((resolve, reject) => {
+		yauzl.open('src/locales/newLocales.zip', {lazyEntries: true}, (err, zipfile) => {
+			if (err) {
+				console.log('Error extracting: ' + err)
+				reject(err);
+				return;
+			}
+
+			zipfile.readEntry();
+			zipfile.on('entry', (entry) => {
+				zipfile.openReadStream(entry, (err, readStream) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+
+					const filePath = path.join('src/locales', entry.fileName);
+					const writeStream = fs.createWriteStream(filePath);
+					readStream.pipe(writeStream);
+
+					writeStream.on('close', () => {
+						zipfile.readEntry();
+					});
+				});
+			});
+
+			zipfile.on('end', () => {
+				resolve();
+			});
+
+			zipfile.on('error', (err) => {
+				console.log('Error extracting: ' + err)
+				reject(err);
+			});
+		});
+	});
+
+	await fs.unlinkSync('src/locales/newLocales.zip')
+}
+
+downloadLocals().then(() => console.log('🚀 Done'))
