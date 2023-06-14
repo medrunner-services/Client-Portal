d0ee610d8a3cf0bcacf16e0833d65e9207a895ee
Elliot Thiebaut <captain@medrunner.dev>
2023-06-13 20:13:00 -0400

    v1.2.2

--- src/utils/discordRedirects.ts
+++ src/utils/discordRedirects.ts
@@ -1,6 +1,6 @@
 export function redirectToDiscordLogin(): void {
     window.location.replace(
-        `https://discord.com/oauth2/authorize?client_id=1050206397972873227&scope=identify&response_type=code&redirect_uri=${
+        `https://discord.com/oauth2/authorize?client_id=${import.meta.env.VITE_DISCORD_CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${
             import.meta.env.VITE_CALLBACK_URL
         }/auth`,
     );
