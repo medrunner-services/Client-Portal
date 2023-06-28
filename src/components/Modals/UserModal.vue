d20a55fa591840d0a786ce1f72ca5bb52e54ecdf
Elliot Thiebaut <captain@medrunner.dev>
2023-06-28 15:14:56 -0400

    CP-128 Add what's new

--- src/components/Modals/UserModal.vue
+++ src/components/Modals/UserModal.vue
@@ -13,6 +13,7 @@ const isInputtingRsiHandle = ref(false);
 const newRsiHandle = ref(userStore.user.rsiHandle);
 const rsiHandleErrorMessage = ref("");
 const rsiHandleUpdating = ref(false);
+const displayFullUpdateNotes = ref(false);
 // eslint-disable-next-line no-undef
 const appVersion = APP_VERSION;
 
@@ -45,46 +46,90 @@ async function updateRsiHandle(): Promise<void> {
 </script>
 
 <template>
-    <div class="mt-2 flex flex-col justify-between lg:flex-row lg:items-center">
-        <input
-            v-if="isInputtingRsiHandle"
-            type="text"
-            v-model="newRsiHandle"
-            class="w-fit flex-grow"
-            :class="[rsiHandleErrorMessage ? 'input-text-error' : 'input-text', userStore.user.personType !== 0 ? 'w-fit' : '']"
-            :disabled="!isInputtingRsiHandle"
-        />
-        <p v-else class="font-Mohave text-3xl font-bold text-primary-900 lg:text-3xl">{{ userStore.user.rsiHandle }}</p>
-        <button
-            v-if="userStore.user.personType === 0"
-            :disabled="rsiHandleUpdating"
-            @click.prevent="updateRsiHandle()"
-            class="mt-3 flex w-fit items-center justify-center border-2 border-primary-900 px-5 py-2 font-Inter font-bold lg:mt-0 lg:px-10"
-            :class="[isInputtingRsiHandle ? 'ml-0 lg:ml-3' : 'ml-0']"
-        >
-            <svg
-                v-if="rsiHandleUpdating"
-                class="mx-5 h-5 w-5 animate-spin text-primary-900"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
+    <div class="border-b border-gray-200 pb-5">
+        <div class="mt-2 flex flex-col justify-between lg:flex-row lg:items-center">
+            <input
+                v-if="isInputtingRsiHandle"
+                type="text"
+                v-model="newRsiHandle"
+                class="w-fit flex-grow"
+                :class="[rsiHandleErrorMessage ? 'input-text-error' : 'input-text', userStore.user.personType !== 0 ? 'w-fit' : '']"
+                :disabled="!isInputtingRsiHandle"
+            />
+            <p v-else class="font-Mohave text-3xl font-bold text-primary-900 lg:text-3xl">{{ userStore.user.rsiHandle }}</p>
+            <button
+                v-if="userStore.user.personType === 0"
+                :disabled="rsiHandleUpdating"
+                @click.prevent="updateRsiHandle()"
+                class="mt-3 flex w-fit items-center justify-center border-2 border-primary-900 px-5 py-2 font-Inter font-bold lg:mt-0 lg:px-10"
+                :class="[isInputtingRsiHandle ? 'ml-0 lg:ml-3' : 'ml-0']"
             >
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-            </svg>
-            <span v-else>{{ isInputtingRsiHandle ? t("form_confirm") : t("form_edit") }}</span>
-        </button>
+                <svg
+                    v-if="rsiHandleUpdating"
+                    class="mx-5 h-5 w-5 animate-spin text-primary-900"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                </svg>
+                <span v-else>{{ isInputtingRsiHandle ? t("form_confirm") : t("form_edit") }}</span>
+            </button>
+        </div>
+        <p v-if="rsiHandleErrorMessage" class="mt-2 w-full text-xs text-primary-400">
+            {{ rsiHandleErrorMessage }}
+        </p>
     </div>
-    <p v-if="rsiHandleErrorMessage" class="mt-2 w-full text-xs text-primary-400">
-        {{ rsiHandleErrorMessage }}
-    </p>
 
     <!--  TODO: Add localization  -->
-    <p @click="$emit('gotoDevView')" class="mt-16 w-fit cursor-pointer font-semibold underline decoration-2">Developer section</p>
+    <div v-auto-animate class="border-b border-gray-200 py-5">
+        <p class="font-Mohave text-2xl font-semibold text-primary-900">What's new</p>
+        <p class="mt-2 font-semibold">New Features ✨</p>
+        <ul class="list-inside list-disc">
+            <li>New logo on different environments</li>
+            <li>Added the version number in the user profile</li>
+            <li>Add German to the available languages</li>
+        </ul>
+        <div v-if="displayFullUpdateNotes">
+            <p class="mt-4 font-semibold">Bug Fixes 🐛</p>
+            <ul class="list-inside list-disc">
+                <li>Dialect fallback is only going to French</li>
+                <li>Scrolling blocked after changing the language on mobile</li>
+            </ul>
+            <p class="mt-4 font-semibold">Improvements 🛠️</p>
+            <ul class="list-inside list-disc">
+                <li>Improved responsive behavior on ultra-side screens</li>
+                <li>Language selector overhaul</li>
+                <li>Added confirm and back button when canceling an emergency</li>
+                <li>Added clear error messages on emergency cancel error</li>
+                <li>Updated cancel emergency screen texts</li>
+                <li>Improved linking screen UI</li>
+            </ul>
+        </div>
+        <p v-else class="mt-2 cursor-pointer font-semibold" @click="displayFullUpdateNotes = true">[...]</p>
+    </div>
+
+    <div class="border-b border-gray-200 py-5">
+        <!--  TODO: Add localization  -->
+        <p @click="$emit('gotoDevView')" class="mb-2 w-fit cursor-pointer font-semibold underline decoration-2">Developer section</p>
+        <div class="w-fit cursor-pointer">
+            <a
+                href="https://www.medrunner.space/legal-information"
+                target="_blank"
+                class="flex w-fit items-center font-semibold underline decoration-2"
+            >
+                <span>Privacy Policy</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="ml-1 h-3 w-3">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+            </a>
+        </div>
+    </div>
 
     <div class="mt-16 lg:flex lg:items-end lg:justify-between">
         <button @click="emit('disconnectUser')" class="button-primary button-48 w-full lg:w-fit">
