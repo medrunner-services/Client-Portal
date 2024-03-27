9661b76eba18bbd5db28bbaef272ee677dd3b0ac
ElliotThiebaut <hello@elliotthiebaut.com>
2024-03-26 20:27:13 -0400

    CP-228 Errors popup in dark mode don't have background

--- src/components/utils/GlobalAlert.vue
+++ src/components/utils/GlobalAlert.vue
@@ -5,7 +5,7 @@ const emit = defineEmits<{
 </script>
 
 <template>
-    <div class="absolute top-14 z-50 mb-4 flex items-center rounded-lg bg-red-50 p-4 text-red-800 dark:bg-gray-800 dark:text-red-400">
+    <div class="absolute top-14 z-50 mb-4 flex items-center rounded-lg bg-red-50 p-4 text-red-800 dark:bg-gray-900 dark:text-red-400">
         <svg class="h-4 w-4 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path
                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
@@ -17,7 +17,7 @@ const emit = defineEmits<{
         <button
             @click="emit('close')"
             type="button"
-            class="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-red-50 p-1.5 text-red-500 hover:bg-red-200 focus:ring-2 focus:ring-red-400 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+            class="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-red-50 p-1.5 text-red-500 hover:bg-red-200 focus:ring-2 focus:ring-red-400 dark:bg-gray-900 dark:text-red-400 dark:hover:bg-gray-700"
         >
             <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
