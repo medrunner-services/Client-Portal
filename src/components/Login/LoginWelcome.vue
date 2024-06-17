6210c53b850945b19ad2e16614738332282b8ecd
ElliotThiebaut <hello@elliotthiebaut.com>
2024-06-16 23:08:25 -0400

    Fixed redirects on login

--- src/components/Login/LoginWelcome.vue
+++ src/components/Login/LoginWelcome.vue
@@ -15,7 +15,7 @@ const isRegistrationEnabled = !!(import.meta.env.VITE_FEATURE_REGISTRATION_ENABL
     <div>
         <h1 class="mt-8 font-Mohave text-3xl font-bold">{{ t("login_welcomeBack") }}</h1>
         <p class="mt-2 font-medium text-gray-500">{{ t("login_welcomeBackHelperText") }}</p>
-        <GlobalButton class="mt-8 w-full" size="full" @click="redirectToDiscordLogin((route.query.redirect as string) ?? '')"
+        <GlobalButton class="mt-8 w-full" size="full" @click="redirectToDiscordLogin(encodeURIComponent(route.query.redirect as string) ?? '')"
             >{{ t("login_logInButton") }}
         </GlobalButton>
         <GlobalButton v-if="isRegistrationEnabled" class="mt-4 w-full" type="secondary" size="full" @click="redirectToDiscordRegister()"
