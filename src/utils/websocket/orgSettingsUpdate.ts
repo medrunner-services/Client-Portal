13fbafc3d0eaa777470a1f9a02b92fa84ae8b322
ElliotThiebaut <hello@elliotthiebaut.com>
2024-12-19 01:03:37 -0500

    CP-277 Org settings WS

--- src/utils/websocket/orgSettingsUpdate.ts
+++ src/utils/websocket/orgSettingsUpdate.ts
@@ -5,7 +5,5 @@ import { useLogicStore } from "@/stores/logicStore.ts";
 export async function orgSettingsUpdate(updatedOrgSettings: OrgSettings) {
     const logicStore = useLogicStore();
 
-    if (updatedOrgSettings.public) {
-        logicStore.medrunnerSettings = updatedOrgSettings.public;
-    }
+    logicStore.medrunnerSettings = updatedOrgSettings.public;
 }
