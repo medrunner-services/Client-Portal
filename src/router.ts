ef073d0f52f3bf4adf78fe5a71b0c8c11fe5175e
ElliotThiebaut <hello@elliotthiebaut.com>
2025-01-07 15:41:25 -0500

    Improved router behavior

--- src/router.ts
+++ src/router.ts
@@ -61,6 +61,18 @@ async function isUserNotLinked(): Promise<string | boolean> {
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else if (to.hash) {
+            return {
+                el: to.hash,
+                behavior: "smooth",
+            };
+        } else {
+            return { top: 0 };
+        }
+    },
     routes: [
         {
             path: "/",
