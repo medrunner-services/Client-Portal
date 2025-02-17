c532be522ffca354a4a5bbe243721ad17861dbf5
ElliotThiebaut <hello@elliotthiebaut.com>
2025-02-16 22:42:03 -0500

    CP-290 Working on message editing

--- src/components/Emergency/ChatMessagesContainer.vue
+++ src/components/Emergency/ChatMessagesContainer.vue
@@ -84,7 +84,6 @@ function getMessageAuthor(message: ChatMessage): string {
 }
 
 function isMessageAuthor(id: string): boolean {
-    console.log("Is message author? ", id === props.user.id);
     return id === props.user.id;
 }
 
@@ -173,7 +172,15 @@ function messageClasses(messageIndex: number, senderId: string): string {
                 >
                     {{ t("tracking_readMore") }}
                 </p>
-                <p class="ml-auto mt-1 text-xs">{{ timestampToHours(message.messageSentTimestamp) }}</p>
+                <p class="ml-auto mt-1 text-xs">
+                    <!--  TODO: localization  -->
+                    <span v-if="message.edited" class="italic">Edited - </span> {{ timestampToHours(message.messageSentTimestamp) }}
+                </p>
+                <svg class="size-3.5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path
+                        d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"
+                    />
+                </svg>
             </div>
         </div>
 
