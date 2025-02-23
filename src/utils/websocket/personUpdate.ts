a25b4f06ebba5ec84f4f78197b5dce6a2e835578
ElliotThiebaut <hello@elliotthiebaut.com>
2025-02-23 16:34:57 -0500

    hotfix: perms not updated after linking

--- src/utils/websocket/personUpdate.ts
+++ src/utils/websocket/personUpdate.ts
@@ -1,10 +1,22 @@
 import { type Person } from "@medrunner/api-client";
+import { HubConnectionState } from "@microsoft/signalr";
 
+import { useLogicStore } from "@/stores/logicStore.ts";
 import { useUserStore } from "@/stores/userStore";
 import type { SyncedSettings } from "@/types.ts";
+import { LocalStorageItems, WSState } from "@/types.ts";
+import { ws } from "@/utils/medrunnerClient.ts";
 
-export function personUpdate(newUser: Person) {
+export async function personUpdate(newUser: Person) {
     const userStore = useUserStore();
+    const logicStore = useLogicStore();
+
+    if (!userStore.user.rsiHandle && newUser.rsiHandle) {
+        await ws.stop();
+        localStorage.removeItem(LocalStorageItems.ACCESS_TOKEN_EXPIRATION);
+        await ws.start();
+        if (ws.state === HubConnectionState.Connected) logicStore.currentWSState = WSState.HEALTHY;
+    }
 
     userStore.user = newUser;
     if (userStore.user.clientPortalPreferencesBlob)
