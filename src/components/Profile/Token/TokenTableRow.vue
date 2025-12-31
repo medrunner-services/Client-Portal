120b11fbf99e092fd7df2b0821b668be877fe7b8
ElliotThiebaut <hello@elliotthiebaut.com>
2025-12-31 12:37:27 +0100

    chore: removed commented code

--- src/components/Profile/Token/TokenTableRow.vue
+++ src/components/Profile/Token/TokenTableRow.vue
@@ -120,34 +120,6 @@ function handleClickOutside(event: MouseEvent) {
                 {{ t("developer_tokenNever") }}
             </p>
         </div>
-
-        <!--        <div -->
-        <!--            class=" -->
-        <!--                col-span-1 flex items-center justify-end font-medium text-red-600 -->
-        <!--                md:col-span-2 -->
-        <!--            " -->
-        <!--        > -->
-        <!--            <div -->
-        <!--                class=" -->
-        <!--                    flex cursor-pointer gap-2 rounded-lg p-2 -->
-        <!--                    hover:bg-red-600/10 -->
-        <!--                " @click="displayDeleteTokenModal = true" -->
-        <!--            > -->
-        <!--                <svg class="h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20"> -->
-        <!--                    <path -->
-        <!--                        d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z" -->
-        <!--                    /> -->
-        <!--                </svg> -->
-        <!--                <p -->
-        <!--                    class=" -->
-        <!--                        hidden -->
-        <!--                        md:block -->
-        <!--                    " -->
-        <!--                > -->
-        <!--                    {{ t("developer_tokenDelete") }} -->
-        <!--                </p> -->
-        <!--            </div> -->
-        <!--        </div> -->
     </div>
 
     <div
